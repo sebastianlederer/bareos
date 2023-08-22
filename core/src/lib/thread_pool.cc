ffb2f830c8f7fb8f90d1c3f42d58755dd1ad206f
Sebastian Sura <sebastian.sura@bareos.com>
2023-08-22 09:45:17 +0200

    [fixup] compiler problems

--- core/src/lib/thread_pool.cc
+++ core/src/lib/thread_pool.cc
@@ -142,7 +142,7 @@ void thread_pool::pool_work(std::size_t id, thread_pool* pool)
 auto thread_pool::dequeue() -> std::optional<task>
 {
   auto locked = queue.lock();
-  locked.wait(queue_or_death, [](std::optional<auto>& queue) {
+  locked.wait(queue_or_death, [](auto& queue) {
     return !queue.has_value() || queue->size() > 0;
   });
   if (!locked->has_value()) { return std::nullopt; }
